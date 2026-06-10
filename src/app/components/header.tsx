2e6320e2b838478529c49bdacf4d7f55849b2443
DuoVZA <zayvlad2009@gmail.com>
2026-06-10 13:26:32 +0300

    h

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return(
-        <div className="bg-zinc-50  w-[1920px] h-[87px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+        <div className="bg-zinc-50  w-full h-[87px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
             <img src="/pictures/invora_logo.png" alt="Invora Logo" className="w-[124px]"/>
         </div>
     )
